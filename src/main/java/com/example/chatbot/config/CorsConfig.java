def1da3281b9d5373dcc4b969580f7fa570d3752
irvanfahreza <irvanfahreza45@gmail.com>
2026-03-06 15:06:07 +0700

    change cors

--- src/main/java/com/example/chatbot/config/CorsConfig.java
+++ src/main/java/com/example/chatbot/config/CorsConfig.java
@@ -15,12 +15,12 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200",
-                                        "https://irvan-v8193y3l3-irvanfahrezas-projects.vercel.app",
-                                        "https://irvan-ai.vercel.app")
+                        .allowedOriginPatterns(
+                                "http://localhost:4200",
+                                "https://*.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowCredentials(false);
             }
         };
     }
